1c1f222865da30765b4290e4de588f94f2f1dcd0
Kohl Listi <kohllisti@riseup.net>
2021-07-20 21:38:00 +0200

    Remove sudo

--- server/installDependencies.sh
+++ server/installDependencies.sh
@@ -4,6 +4,6 @@ apk add docker
 # docker compose dependencies
 apk add py-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make
 # fetch docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # make docker-compose executable
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
