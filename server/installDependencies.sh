abced211d04aa5424cac4d090045617fce9af7bf
Kohl Listi <kohllisti@riseup.net>
2021-07-27 19:19:18 +0200

    Add install of curl

--- server/installDependencies.sh
+++ server/installDependencies.sh
@@ -2,7 +2,7 @@
 apk add git
 apk add docker
 # docker compose dependencies
-apk add py-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make
+apk add py-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make curl
 # fetch docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # make docker-compose executable
