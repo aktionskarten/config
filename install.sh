b541985371eb1fc1490bc604d6fe9b7544e7a266
Kohl Listi <kohllisti@riseup.net>
2021-10-19 23:01:07 +0200

    Do not ask to install npm

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ systemctl daemon-reload
 
 # set up frontend
 cd $SCRIPT_DIR/frontend
-yum install npm
+yum install -y npm
 npm install
 echo "API_ENDPOINT=https://backend.aktionskarten.org" > .env
 npm build
