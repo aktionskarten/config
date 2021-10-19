27bbbc195416fe0aa44671962cf7d55ba3db40e7
Kohl Listi <kohllisti@riseup.net>
2021-10-19 23:27:04 +0200

    Do not install puppeteer

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ systemctl daemon-reload
 # set up frontend
 cd $SCRIPT_DIR/frontend
 yum install -y npm
-npm install
+PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="1" npm install
 echo "API_ENDPOINT=https://backend.aktionskarten.org" > .env
 npm run build
 
