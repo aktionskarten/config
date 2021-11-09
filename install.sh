6cb540a07d5c1fc5068364be656bfb6e2bb9394e
Kohl Listi <kohllisti@riseup.net>
2021-11-09 20:16:30 +0100

    Make URLS configurable

--- install.sh
+++ install.sh
@@ -2,54 +2,61 @@
 
 set -ex
 
-SCRIPT_DIR=$(dirname $(realpath "$0"))
-PWD_DIR=$(pwd)
+CERTBOT_EMAIL="kontakt@aktionskarten.org"
 
-# stop aktionskarten if already running
+BACKEND_URL="backend.aktionskarten.org"
+TILES_URL="tiles.aktionksarten.org"
+FRONTEND_URL="frontend.aktionksarten.org"
+
+
+### get directories
+SCRIPT_DIR=$(dirname $(realpath "$0"))
+
+### stop aktionskarten if already running
 systemctl stop docker-compose@aktionskarten || true
 
-# install docker
+### install docker
+# add docker repo
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# install and start/enable docker
 yum install -y docker-ce docker-ce-cli containerd.io
 systemctl start docker
 systemctl enable docker
 
-# install docker-compose
-yum install -y curl
 
+### install docker-compose
+yum install -y curl
 rm -f /usr/local/bin/docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-cd $SCRIPT_DIR
-
-# checkout subrepos
+### checkout subrepos
 git pull --recurse-submodules
 git submodule update --init --recursive
 
-# set up systemctl docker script
+### set up systemctl docker script
 mkdir -p /etc/docker/compose
 ln -sf $SCRIPT_DIR/server/docker-compose@.service /etc/systemd/system/docker-compose@.service
 ln -sf $SCRIPT_DIR/ /etc/docker/compose/aktionskarten
-
 systemctl daemon-reload
 
-# set up frontend
-yum install -y npm
-
+### set up frontend
 cd $SCRIPT_DIR/frontend
+yum install -y npm
 rm -rf node_modules
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="1" npm install --quiet
 
-echo "API_ENDPOINT=https://backend.aktionskarten.org" > .env
+echo "API_ENDPOINT=https://$BACKEND_URL" > .env
 npm run --silent build
 
 mkdir -p /var/www/aktionskarten-frontend/
 mv dist/* /var/www/aktionskarten-frontend/
 chown -R nginx:nginx /var/www/
 
-# install nginx
+cd $SCRIPT_DIR
+
+### install nginx
 yum install -y epel-release
 yum install -y nginx certbot python3-certbot-nginx
 cp nginx/*.conf /etc/nginx/conf.d/
@@ -57,10 +64,9 @@ cp nginx/*.conf /etc/nginx/conf.d/
 systemctl enable nginx
 systemctl restart nginx
 
-certbot -n --agree-tos --email=kontakt@aktionskarten.org --nginx -d tiles.aktionskarten.org
-certbot -n --nginx -d backend.aktionskarten.org
-certbot -n --nginx -d frontend.aktionskarten.org
+certbot -n --agree-tos --email=$CERTBOT_EMAIL --nginx -d $TILES_URL
+certbot -n --nginx -d $BACKEND_URL
+certbot -n --nginx -d $FRONTEND_URL
 
 systemctl start docker-compose@aktionkarten
 
-cd $PWD_DIR
