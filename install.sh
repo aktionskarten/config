198e27f02a5dc17c2ea0698c6413333a06aa59e6
Kohl Listi <kohllisti@riseup.net>
2021-11-12 21:39:05 +0100

    Make git and curl quiet

--- install.sh
+++ install.sh
@@ -27,12 +27,12 @@ systemctl enable docker
 ### install docker-compose
 yum install -y curl -q
 rm -f /usr/local/bin/docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -sS -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 ### checkout subrepos
-git submodule update --init --recursive
-git pull --recurse-submodules
+git submodule update --init --recursive --quiet
+git pull --recurse-submodules --quiet
 
 ### set up systemctl docker script
 mkdir -p /etc/docker/compose
